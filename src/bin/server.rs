49f63782e8f1d03523311d8d0e4e44456633c2d7
noot <elizabethjbinks@gmail.com>
2022-09-24 12:43:33 -0400

    implement transaction submission

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,5 +1,8 @@
 use anyhow::{anyhow, Error};
-use ethers::{prelude::*, utils::rlp::{Decodable, Rlp}};
+use ethers::{
+    prelude::*,
+    utils::rlp::{Decodable, Rlp},
+};
 use futures::{sink::SinkExt, stream::StreamExt};
 use nym_websocket::responses::ServerResponse;
 use tokio::net::TcpStream;
@@ -11,16 +14,17 @@ pub const DEFAULT_NYM_CLIENT_ENDPOINT: &str = "ws://localhost:1977";
 
 /// Server maintains a connection to a Nym client and upon receiving an Ethereum
 /// transaction, it submits to an Ethereum node.
-pub struct Server {
+pub struct Server<M: Middleware + 'static> {
     ws: WebSocketStream<TcpStream>,
+    m: M,
 }
 
-impl Server {
-    pub async fn new(endpoint: Option<String>) -> Result<Self, Error> {
+impl<M: Middleware + 'static> Server<M> {
+    pub async fn new(endpoint: Option<String>, m: M) -> Result<Self, Error> {
         let (ws, _) =
             connect_async(endpoint.unwrap_or(DEFAULT_NYM_CLIENT_ENDPOINT.to_string())).await?;
 
-        Ok(Server { ws })
+        Ok(Server { ws, m })
     }
 
     pub async fn send_address_request(&mut self) -> Result<(), Error> {
@@ -53,14 +57,40 @@ impl Server {
                 }
             };
 
-            let _transaction_res = decode_transaction(&msg_bytes.message);
+            let transaction_res = decode_transaction(&msg_bytes.message);
+            if transaction_res.is_err() {
+                warn!("{:?}", transaction_res.err());
+                continue;
+            }
+
+            let receipt_res = self.submit_transaction(&transaction_res.unwrap()).await;
+            if receipt_res.is_err() {
+                warn!("{:?}", receipt_res.err());
+                continue;
+            }
+
+            info!("transaction included: {:?}", receipt_res.unwrap());
         }
+    }
+
+    pub async fn submit_transaction(
+        &self,
+        transaction: &Transaction,
+    ) -> Result<TransactionReceipt, Error> {
+        let pending_tx = self.m.send_transaction(transaction, None).await?;
+        info!("submitted transaction: hash {:?}", pending_tx.tx_hash());
+        let maybe_receipt = pending_tx.await?;
+        if maybe_receipt.is_none() {
+            return Err(anyhow!("did not receive transaction receipt"));
+        }
+        Ok(maybe_receipt.unwrap())
     }
 }
 
 fn decode_transaction(bytes: &[u8]) -> Result<Transaction, Error> {
     let rlp = Rlp::new(bytes);
-    Transaction::decode(&rlp).map_err(|e| anyhow!("failed to decode transaction from message: {:?}", e))
+    Transaction::decode(&rlp)
+        .map_err(|e| anyhow!("failed to decode transaction from message: {:?}", e))
 }
 
 fn parse_nym_message(msg: Message) -> Result<ServerResponse, Error> {
